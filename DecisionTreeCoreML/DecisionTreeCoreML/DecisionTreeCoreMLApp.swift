516adf6cd8242cd014b4edefe4d417db1fc05a17
longzhao-gd <long.zhao@greendotcorp.com>
2022-08-16 13:42:32 +0800

    init DecisionTree project

---
+++ DecisionTreeCoreML/DecisionTreeCoreML/DecisionTreeCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecisionTreeCoreMLApp.swift
+//  DecisionTreeCoreML
+//
+//  Created by Long Zhao on 2022/8/16.
+//
+
+import SwiftUI
+
+@main
+struct DecisionTreeCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
