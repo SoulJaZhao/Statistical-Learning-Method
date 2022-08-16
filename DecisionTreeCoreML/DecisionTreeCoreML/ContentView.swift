ba5de3f68bd59a44d938d7e281fcf50e7b0c7544
longzhao-gd <long.zhao@greendotcorp.com>
2022-08-16 14:26:59 +0800

    Complete DecisionTree Demo

--- DecisionTreeCoreML/DecisionTreeCoreML/ContentView.swift
+++ DecisionTreeCoreML/DecisionTreeCoreML/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var age: Int = 0
+    @State private var height: Int = 0
+    @State private var weight: Int = 0
+    @State private var salary: Int = 0
+    @State private var faceScores: [Int] = [0, 50, 100]
+    @State private var selectedFaceScore: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Image(uiImage: UIImage.init(systemName: "circle")!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.red)
+                .frame(width: 200, height: 200, alignment: .top)
+                .padding()
+            
+            Form {
+                HStack {
+                    Text("Age:")
+                    TextField("age", value: $age, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.numberPad)
+                }
+                
+                HStack {
+                    Text("Height:")
+                    TextField("Height", value: $height, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.numberPad)
+                }
+                
+                HStack {
+                    Text("Weight:")
+                    TextField("Weight", value: $weight, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.numberPad)
+                }
+                HStack {
+                    Text("Salary:")
+                    TextField("Salary", value: $salary, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                        .keyboardType(.numberPad)
+                }
+                HStack {
+                    Text("Face score:")
+                    Picker("Face score", selection: $selectedFaceScore) {
+                        ForEach(faceScores, id:\.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
             .padding()
+            
+            Button("Match") {
+                print("match")
+            }
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
+        }
     }
 }
 
